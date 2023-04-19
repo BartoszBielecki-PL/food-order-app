925978bf77cf3a67851fba918aef8a670aede3c2
BartoszBielecki-PL <bartosz.m.bielecki@gmail.com>
2023-04-19 17:53:35 +0200

    create CartProvider component

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import CartIcon from '../Cart/CartIcon';
+import CartContext from '../../store/cart-context';
 
 import classes from './HeaderCartButton.module.css';
 
 
 const HeaderCartButton = (props) => {
+    const cartCtx = useContext(CartContext);
+
+    const numberOfCartItems = cartCtx.items.reduce((currentItem, item) => {
+        return currentItem + item.amount;
+    }, 0)
 
     return (
         <button className={classes.button} onClick={props.onClick}>
@@ -14,7 +20,7 @@ const HeaderCartButton = (props) => {
             </span>
             <span>Your Cart</span>
             <span className={classes.badge}>
-                3
+                {numberOfCartItems}
             </span>
         </button>
     )
